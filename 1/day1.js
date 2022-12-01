94a09b9f2db9e24a45925d480e756a6ff3770383
bhagat singh <bhagatsinghsinghsingh1998@gmail.com>
2022-12-01 14:35:19 +0530

    1 day 1

---
+++ 1/day1.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+let max = 0;
+let total = 0;
+try {
+  const data = fs.readFileSync('input.txt').toString()
+  data.split("\r\n\r\n").forEach(each => {
+    const cals = each.split('\r\n');
+    let total = 0;
+    cals.forEach(cal => {
+      total += +cal;
+    })
+    // console.log('tottal :: ', total);
+    if(total > max) {
+      max = total
+    }
+  })
+} catch (e) {
+  console.log('Error:', e.stack);
+}
+
+console.log('max :: ', max);
